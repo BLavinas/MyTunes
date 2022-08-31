8c05098689eabdf55ecb769c234e60040960eb43
Bernardo Lavinas <bernardorclavinas@gmail.com>
2022-08-31 18:04:31 -0300

    Requisito 5 realizado

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,56 @@ import React from 'react';
 import Header from '../Components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isBtnDisabled: true,
+      searchArtist: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => {
+      const { searchArtist } = this.state;
+      const minInputName = 2;
+      if (searchArtist.length >= minInputName) {
+        this.setState({
+          isBtnDisabled: false,
+        });
+      } else {
+        this.setState({
+          isBtnDisabled: true,
+        });
+      }
+    });
+  };
+
   render() {
+    const { isBtnDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <label htmlFor="searchArtist">
+            <input
+              name="searchArtist"
+              onChange={ this.handleChange }
+              type="text"
+              data-testid="search-artist-input"
+            />
+          </label>
+          <button
+            type="button"
+            disabled={ isBtnDisabled }
+            data-testid="search-artist-button"
+          >
+            Pesquisar
+
+          </button>
+        </form>
       </div>
     );
   }
