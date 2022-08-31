a58d49b12c9cebe9b17d3f6a1089545f9600a464
Bernardo Lavinas <bernardorclavinas@gmail.com>
2022-08-31 17:17:32 -0300

    Requisito 4 realizado

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Loading from '../pages/Loading';
 import { getUser } from '../services/userAPI';
 
@@ -27,6 +28,9 @@ export default class Header extends Component {
         <p data-testid="header-user-name">
           {receivedName}
         </p>
+        <Link to="/search" data-testid="link-to-search">Search</Link>
+        <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+        <Link to="/profile" data-testid="link-to-profile">Profile</Link>
       </header>
     );
   }
